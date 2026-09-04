68c298727349e381e3aa7e509d2be40140877375
Sunil Kumar <skmorwa2001@gmail.com>
2026-09-04 15:44:58 +0530

    Input Section

--- app.py
+++ app.py
@@ -21,3 +21,66 @@ st.write("Enter pollutant values below to estimate the US AQI"
          "using the trained Gradient Boosting Model"
 )
 st.divider()
+
+# Input Section
+
+st.header("Air Quality Parameters")
+
+col1,col2,col3 = st.columns(3)
+
+with col1:
+    pm10 = st.number_input(
+        "PM10",
+        min_value=0.0,
+        value=75.0
+    )
+
+    pm2_5= st.number_input(
+        "PM2.5",
+        min_value=0.0,
+        value=37.0
+    )
+
+    carbon_monoxide= st.number_input(
+        "Carbon Monoxide",
+        min_value=0.0,
+        value=443.0
+    )
+
+with col2:
+    nitrogen_dioxide= st.number_input(
+        "Nitrogen Dioxide",
+        min_value=0.0,
+        value=18.0
+    )    
+
+    sulphur_dioxide= st.number_input(
+        "Sulphur Dioxide",
+        min_value=0.0,
+        value=8.8
+    )
+
+    ozone= st.number_input(
+        "Ozone",
+        min_value=0.0,
+        value=82.0
+    )
+
+with col3:
+    aersol_optical_depth= st.number_input(
+        "Aersol Optical Depth",
+        min_value=0.0,
+        value=0.2
+    )    
+
+    dust= st.number_input(
+        "Dust",
+        min_value=0.0,
+        value=20.0
+    )
+
+    uv_index=st.number_input(
+        "UV Index",
+        min_value=0.0,
+        value=5.0
+    )
