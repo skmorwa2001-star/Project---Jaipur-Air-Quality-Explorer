5a0dc908b4919e2a6157abbb8c7675c5000e6e0d
Sunil Kumar <skmorwa2001@gmail.com>
2026-09-04 15:33:43 +0530

    Added Title

--- app.py
+++ app.py
@@ -11,3 +11,13 @@ st.set_page_config(
 # Load Model
 
 model=joblib.load("gradient_boosting_aqi_model.pkl")
+
+# Title
+
+st.title("Jaipur Air Quality Explorer")
+st.subheader("Explore and Estimate Jaipur's Air Quality")
+
+st.write("Enter pollutant values below to estimate the US AQI"
+         "using the trained Gradient Boosting Model"
+)
+st.divider()
